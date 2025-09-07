0c1ce6c3de55f4339069f1c0d52565bdedc87569
significant-cijfer <fake.potsjem@gmail.com>
2025-09-07 22:46:19 +0200

    First comitty witty

---
+++ main.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+	void main(String[] main) {
+		String lang = "HolyJ!";
+
+		System.out.println("Greetings from HolyJ!");
+
+		System.out.println("I hope you enjoy this demo!");
+		System.out.println("Sincerely, SG, creator of " + lang);
+	}
+}
